3714abae950c1bef8f0ec1a85965d2c62ffd51d4
Dilane Christian <dilanechristian2@gmail.com>
2025-03-01 17:30:15 +0100

    This commit fixes #13

--- todo_site/todo/views.py
+++ todo_site/todo/views.py
@@ -2,6 +2,7 @@ from datetime import timedelta, datetime
 from django.shortcuts import render,redirect
 from django.contrib import messages
 from dateutil.relativedelta import relativedelta
+from django.db.models import Q # Used to search simulataneously on multiple fields of a table in the database/model
 
 # import todo form and models
 from .forms import TodoForm
@@ -38,15 +39,39 @@ def create_recurring_tasks(task, start_date, frequency):
     return None
         
 def index(request):
-    # Get the selected tag from the query parameters, if any
+    # Get query parameters
     selected_tag = request.GET.get('tag', None)
+    search_query = request.GET.get("search", "")
+    priority_filter = request.GET.get("priority", "")
+    sort_option = request.GET.get("sort", "")
     
-    # Filter tasks by the selected tag if present and ordering by priority then date
+    # Base queryset
+    item_list = Todo.objects.all()
+    
+    # Filter tasks
     if selected_tag:
         item_list = Todo.objects.filter(tags__name=selected_tag).order_by('-priority', 'due_date', '-date')
-    else:
-        item_list = Todo.objects.order_by('-priority', 'due_date', '-date')
     
+    if search_query:
+        item_list = item_list.filter(Q(title__icontains=search_query) | Q(details__icontains=search_query))
+    
+    if priority_filter:
+        item_list = item_list.filter(priority=priority_filter)
+    
+    # Sorting logic
+    sort_fields = {
+        "title": "title",
+        "-title": "-title",
+        "due_date": "due_date",
+        "-due_date": "-due_date",
+        "priority": "priority",
+        "-priority": "-priority",
+    }
+    if sort_option in sort_fields:
+        item_list = item_list.order_by(sort_fields[sort_option])
+    else:
+        item_list = item_list.order_by('-priority', 'due_date', '-date')
+        
     # Handle form submission
     if request.method == "POST":
         form = TodoForm(request.POST)
